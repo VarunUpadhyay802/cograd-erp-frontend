4f35a70676a897b5ff1ae9c7761f3d39fc1c33fc
varun__upadhyay <varun.802.vu@gmail.com>
2024-04-19 17:16:35 +0530

    login and reg form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import HomePage from "./HomePage";
+import SchoolLogin from "./pages/SchoolLogin";
+import SchoolRegister from "./pages/SchoolRegister";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} /> */}
+        <Route path="/" element={<SchoolLogin />} />
+        <Route path="/schoolRegister" element={<SchoolRegister />} />
+        <Route path="/schoolLogin" element={<SchoolLogin />} />
       </Routes>
     </>
   );
---
+++ src/pages/SchoolRegister.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+const SchoolRegister = () => {
+  const [school, setSchool] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const registerHandler = async (e) => {
+    e.preventDefault(); // Prevent default form submission behavior
+
+    try {
+      const data = await axios.post("/school/register", {
+        school,
+        email,
+        password,
+      });
+      console.log("Registration successful:", data);
+    } catch (error) {
+      console.error("Error registering:", error);
+    }
+  };
+
+  return (
+    <>
+      <section className="flex h-screen flex-col md:flex-row justify-center space-y-10 md:space-y-0 md:space-x-16 items-center my-2 mx-5 md:mx-0 md:my-0">
+        <div className="md:w-1/3 max-w-sm">
+          <img
+            src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+            alt="Sample image"
+          />
+        </div>
+        <div className="md:w-1/3 max-w-sm flex flex-col gap-2 ">
+          <input
+            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
+            type="text"
+            placeholder="School Name"
+            value={school}
+            onChange={(e) => {
+              setSchool(e.target.value);
+            }}
+          />
+          <input
+            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded"
+            type="text"
+            placeholder="Email Address"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            className="text-sm w-full px-4 py-2 border border-solid border-gray-300 rounded "
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+          <div className="mt-4 flex justify-between font-semibold text-sm">
+            <label className="flex text-slate-500 hover:text-slate-600 cursor-pointer">
+              <input className="mr-1" type="checkbox" />
+              <span>Remember Me</span>
+            </label>
+            <a
+              className="text-blue-600 hover:text-blue-700 hover:underline hover:underline-offset-4"
+              href="#"
+            >
+              Forgot Password?
+            </a>
+          </div>
+          <div className="text-center md:text-left">
+            <button
+              className="mt-4 bg-blue-600 hover:bg-blue-700 px-4 py-2 text-white uppercase rounded text-xs tracking-wider"
+              type="submit"
+              onClick={registerHandler}
+            >
+              Register
+            </button>
+          </div>
+          <div className="mt-4 font-semibold text-sm text-slate-500 text-center md:text-left">
+            Don't have an account?{" "}
+            <Link
+              className="text-red-600 hover:underline hover:underline-offset-4"
+              to={"/principalRegister"}
+            >
+              Register
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default SchoolRegister;
