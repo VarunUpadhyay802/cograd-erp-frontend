7ef8c6833b2f1e64c6212d168b6404aecf251c97
varun__upadhyay <varun.802.vu@gmail.com>
2024-05-07 23:49:42 +0530

    working on teacher attendance table

--- src/components/Teachers/TeacherAttendanceData.jsx
+++ src/components/Teachers/TeacherAttendanceData.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
+import AttendanceReport from "../Students/SingleStudent/AttendanceReport";
+import AttendanceReportTable from "../Students/SingleStudent/AttendanceReportTable";
 
 const TeacherAttendanceData = () => {
   return (
     <div>
-      pending....
+      <div className="text-2xl font-bold mb-4">Attendance Report</div>
+      <AttendanceReport width={400} height={200} />
+      <AttendanceReportTable />
     </div>
-  )
-}
+  );
+};
 
-export default TeacherAttendanceData
+export default TeacherAttendanceData;
